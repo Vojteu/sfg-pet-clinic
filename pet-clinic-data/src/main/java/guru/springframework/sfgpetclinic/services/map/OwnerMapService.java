e8388f0dc78fcf39232a2897349b5fcb7c284dce
Vojteu <wojtasss821@gmail.com>
2023-04-03 12:20:27 +0200

    Implemented Map impl for Visit Service. Closes #48.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -11,7 +11,6 @@ import java.util.Set;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
-
     private final PetTypeService petTypeService;
     private final PetService petService;
 
@@ -19,7 +18,6 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
         this.petTypeService = petTypeService;
         this.petService = petService;
     }
-
 
     @Override
     public Set<Owner> findAll() {
@@ -40,7 +38,6 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                         if(pet.getPetType().getId() == null){
                             pet.setPetType(petTypeService.save(pet.getPetType()));
                         }
-
                     }
                     else{
                         throw new RuntimeException("Pet Type is required");
